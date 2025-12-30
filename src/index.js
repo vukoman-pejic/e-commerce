820d66dd4dbcb8942b58ca44be6da1eec43cf9ec
Vukoman Pejic <vukpe@LAPTOP-60LSV4G0.(none)>
2025-12-30 14:49:51 +0100

    feat: create initial database schema with users, products, purchases, bonus payouts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express, { json } from "express";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send("API radi 🚀");
