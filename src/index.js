3ca9823da62389b75651f117ea00ab164370889c
Vukoman Pejic <vukpe@LAPTOP-60LSV4G0.(none)>
2025-12-30 13:08:39 +0100

    initial e-commerce express project setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API radi 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server pokrenut na portu ${PORT}`);
+});
